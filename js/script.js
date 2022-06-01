f932bf593f63de072aab2481e9391ce3d32940b3
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-06-01 22:28:13 +0200

    Made the nav

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const contactButton = document.querySelector("#contact");
+const aboutButton = document.querySelector("#about");
+const aboutMe = document.querySelector(".about");
+const contactMe = document.querySelector(".contact");
+
+
+function showAbout() {
+  aboutMe.classList.add("open");
+}
+
+function closeAbout() {
+  aboutMe.classList.remove("open");
+}
+
+function showContact() {
+  contactMe.classList.add("open");
+}
+
+function closeContact() {
+  contactMe.classList.remove("open");
+}
+
+showAbout();
+showContact();
+closeAbout();
+closeContact();
+
+//https://medium.com/allenhwkim/close-div-when-clicking-outside-it-97255c20a221
