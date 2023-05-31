5172516bcf27c04e713847537b6510f40ec48a6e
Jenny Feragen <jenny@feragen.no>
2023-05-31 17:21:51 +0200

    added sass and some styles

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,29 @@
+
+<div>
+  
+<h1>Jenny Feragen</h1>
+
+<h2>Front End Developer</h2>
+
+</div>
+
+<style lang="scss">
+
+div {
+    color: var(--primary);
+    min-height: 90vh;
+    text-align: center;
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: var(--font-size-xxxl);
+  }
+
+  h2 {
+    font-size: var(--font-size-xl);
+  }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "../lib/styles/styles.scss"
+  import Hero from "../lib/components/Hero.svelte"
+</script>
+
+<main>
+  <Hero/>
+</main>
+
